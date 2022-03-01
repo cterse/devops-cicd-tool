3c44bfbb2915ad14612e1fc86ca6fafff23caf2f
Himanshu Gupta <himanshu199586@gmail.com>
2022-03-01 16:44:30 -0500

    M1 init js file

---
+++ commands/m1init.js
@@ -0,0 +1,35 @@
+const chalk = require('chalk');
+const { shellExecSync } = require('../exec/utils');
+const path = require('path');
+
+const PropertiesReader = require('properties-reader');
+const prop = PropertiesReader(path.resolve(path.join(__dirname, '..', 'config', 'app.properties')));
+
+const CONTAINER_NAME = prop.get('container.name');
+const CONTAINER_IMAGE = prop.get('image.name');
+const ID_KEY_PATH = prop.get('ssh.identityfile');
+
+exports.m1init = async () => {
+
+    console.log(chalk.green("Preparing computing environment..."));
+
+    // Start container, stop if container with same name is already up.
+    console.log(chalk.green(`Starting ubuntu:focal instance with name: ${CONTAINER_NAME}`));
+
+    if (prop.get('init.pull_image')) {
+        shellExecSync(`vm pull ${CONTAINER_IMAGE}`);
+        console.log(chalk.green(`Image pull successful.`));
+    }
+
+    if (prop.get('init.restart_container')) {
+        shellExecSync(`vm stop ${CONTAINER_NAME}`);
+        shellExecSync(`vm start ${CONTAINER_NAME} ${CONTAINER_IMAGE}`);
+
+        console.log(chalk.green(`Started successfully...`));
+    }
+
+    shellExecSync(`vm ssh-config ${CONTAINER_NAME}`);
+
+    console.log(chalk.green(`Identity File: ${ID_KEY_PATH}`));
+    
+};
