160bf77e4c7b04c54430c9c30f4129069c689b90
Jayaty <jayaty1607@gmail.com>
2022-03-01 23:36:38 -0500

    Env file

--- commands/m1init.js
+++ commands/m1init.js
@@ -1,12 +1,13 @@
 const chalk = require('chalk');
 const { shellExecSync } = require('../exec/utils');
 const path = require('path');
+require('dotenv').config({path:path.join( path.dirname(require.main.filename), '.env')});
 
 const PropertiesReader = require('properties-reader');
 const prop = PropertiesReader(path.resolve(path.join(__dirname, '..', 'config', 'app.properties')));
 
 const CONTAINER_NAME = prop.get('container.name');
-const CONTAINER_IMAGE = prop.get('m1.image.name');
+const CONTAINER_IMAGE = process.env.M1_VM_IMAGE_VERSION;
 const ID_KEY_PATH = prop.get('ssh.identityfile');
 
 exports.m1init = async () => {
@@ -16,12 +17,13 @@ exports.m1init = async () => {
     // Start container, stop if container with same name is already up.
     console.log(chalk.green(`Starting ubuntu:focal instance with name: ${CONTAINER_NAME}`));
 
-    if (prop.get('init.pull_image')) {
+  
+    if (process.env.INIT_PULL_IMAGE === 'true') {
         shellExecSync(`vm pull ${CONTAINER_IMAGE}`);
         console.log(chalk.green(`Image pull successful.`));
     }
 
-    if (prop.get('init.restart_container')) {
+    if (process.env.INIT_RESTART_CONTAINER === 'true') {
         shellExecSync(`vm stop ${CONTAINER_NAME}`);
         shellExecSync(`vm start ${CONTAINER_NAME} ${CONTAINER_IMAGE}`);
 
@@ -30,6 +32,9 @@ exports.m1init = async () => {
 
     shellExecSync(`vm ssh-config ${CONTAINER_NAME}`);
 
-    console.log(chalk.green(`Identity File: ${ID_KEY_PATH}`));
+    console.log(chalk.green(`Identity File: ${ID_KEY_PATH}\n`));
     
+    console.log(chalk.green(`Shared dirs:`));
+    console.log(chalk.green(`.:~/shared/cwd`));
+    console.log(chalk.green(`~:~/shared/home`));
 };
