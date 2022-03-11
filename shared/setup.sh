9302735e25c1a170c726b524c162361649ce936e
Chinmay Terse <cterse@gmail.com>
2022-03-11 10:40:25 -0500

    Remove Ansible PPA repo after install

--- shared/setup.sh
+++ shared/setup.sh
@@ -5,7 +5,8 @@ set -x
 # exit when any command fails
 set -e
 
+sudo add-apt-repository --remove ppa:ansible/ansible
 sudo apt-get update
 sudo add-apt-repository ppa:ansible/ansible
 sudo apt-get install ansible -y
-
+sudo add-apt-repository --remove ppa:ansible/ansible
