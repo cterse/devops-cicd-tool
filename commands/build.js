9b4478442c16332109347778a1bb6e99d46daa05
Chinmay Terse <cterse@gmail.com>
2022-03-11 18:20:09 -0500

    Changes to take build name and build.yml file for pipeline build

--- commands/build.js
+++ commands/build.js
@@ -2,19 +2,21 @@ const { getProcessor } = require('../exec/utils');
 const m1build = require('./m1build');
 const intelbuild = require('./intelbuild');
 
-exports.command = 'build';
+exports.command = 'build <jobName> <buildFile>';
 exports.desc = 'Build VM and execute jobs';
 exports.builder = yargs => {
     yargs.options({
     });
 };
 
-exports.handler = async () => {
+exports.handler = async argv => {
+
     let processor = getProcessor();
-    console.log(processor);
+    // console.log(processor);
+
     if (processor == 'Arm64')
     {
-        m1build.m1build() 
+        m1build.m1build(argv); 
     }
     else if(processor == 'Intel/Amd64')
     {
--- commands/m1build.js
+++ commands/m1build.js
@@ -2,6 +2,8 @@ const chalk = require('chalk');
 const { shellExecSync, vmSSHExecSync } = require('../exec/utils');
 const path = require('path');
 require('dotenv').config({path:path.join( path.dirname(require.main.filename), '.env')});
+const yaml = require('js-yaml');
+const fs = require('fs');
 
 const PropertiesReader = require('properties-reader');
 const prop = PropertiesReader(path.resolve(path.join(__dirname, '..', 'config', 'app.properties')));
@@ -13,11 +15,32 @@ const ID_KEY_PATH = prop.get('ssh.identityfile');
 const NCSU_GITHUB_USER_TOKEN = process.env.NCSU_GITHUB_PERSONAL_TOKEN
 const SPRING_DB_PASS = process.env.SPRING_DB_PASS
 
-exports.m1build = async () => {
+exports.m1build = async argv => {
+    const { jobName, buildFile, processor } = argv;
 
-    vmSSHExecSync(`'ansible-playbook /home/ubuntu/shared/cwd/shared/build2.yml --extra-vars \\"usertoken=${NCSU_GITHUB_USER_TOKEN} db_pass=${SPRING_DB_PASS}\\"'`, CONTAINER_NAME);
+    let fileContents = fs.readFileSync(path.resolve(__dirname, `../shared/${buildFile}`), 'utf8');
+    data = yaml.load(fileContents);
+
+    let setup = data['setup'];
+    let job = data['jobs'].filter(d => d.name === jobName)[0];
+    let steps = job['steps'];
+
+    // vmSSHExecSync(`'ansible-playbook /home/ubuntu/shared/cwd/shared/build2.yml --tags \\"other-build\\" --extra-vars \\"usertoken=${NCSU_GITHUB_USER_TOKEN} db_pass=${SPRING_DB_PASS}\\"'`, CONTAINER_NAME);
+
+    for (setupRole in setup) {
+        vmSSHExecSync(`'ansible-playbook /home/ubuntu/shared/cwd/shared/playbook.yml  \
+                            --extra-vars \\"choice=${data['setup'][setupRole]}  \
+                            usertoken=${NCSU_GITHUB_USER_TOKEN} db_pass=${SPRING_DB_PASS}\\"'` 
+                            , CONTAINER_NAME);
+    }
     
-    vmSSHExecSync(`'sudo mvn -f /home/ubuntu/iTrust/iTrust2/pom.xml -B -U clean test'`, CONTAINER_NAME);
+    for (buildRole in steps) {
+        vmSSHExecSync(`'ansible-playbook /home/ubuntu/shared/cwd/shared/playbook.yml  \
+                        --extra-vars \\"choice=${steps[buildRole]['run']}  \
+                        usertoken=${NCSU_GITHUB_USER_TOKEN} db_pass=${SPRING_DB_PASS}\\"'`, CONTAINER_NAME);
+    }
+
+    // vmSSHExecSync(`'sudo mvn -f /home/ubuntu/iTrust/iTrust2/pom.xml -B -U clean test'`, CONTAINER_NAME);
 };
 
 
